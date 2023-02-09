8d310d146b5d775d1f2be8ae353d1b3948aae91b
johnacogan67 <johnacogan67@gmail.com>
2023-02-09 10:48:08 +0000

    Initial Commit

---
+++ tesa/tesaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tesaApp.swift
+//  tesa
+//
+//  Created by John A. Cogan on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct tesaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
